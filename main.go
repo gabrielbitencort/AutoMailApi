9468c7a49cbaeaed877ca1dcde77a1f021d39514
Gabriel <gabrielbiten.henrique@gmail.com>
2023-12-16 21:45:35 -0300

    feat: add main script and userManager schema

---
+++ main.go
@@ -0,0 +1,20 @@
+package main
+
+import "github.com/Gabriel-Bitencort/AutoMailApi.git/config"
+
+var (
+	logger *config.Logger
+)
+
+func main() {
+	logger = config.GetLogger("main")
+
+	// Initialize Configs
+	err := config.Init()
+	if err != nil {
+		logger.Errorf("config initialization error: %v", err)
+		return
+	}
+
+	// Initialize Router
+}
--- schemas/userManager.go
+++ schemas/userManager.go
@@ -1,7 +1,25 @@
 package schemas
 
-type UserManager struct {
+import (
+	"gorm.io/gorm"
+	"time"
+)
+
+type UserRegister struct {
+	gorm.Model
+	UserId       string
+	Name         string
+	Email        string
+	PasswordHash string
 }
 
-type UserManagerResponse struct {
+type UserRegisterResponse struct {
+	ID           uint      `json:"id"`
+	CreatedAt    time.Time `json:"createdAt"`
+	UpdatedAt    time.Time `json:"updatedAt"`
+	DeletedAt    time.Time `json:"deletedAt,omitempty"`
+	UserId       string    `json:"user_id"`
+	Name         string    `json:"name"`
+	Email        string    `json:"email"`
+	PasswordHash string    `json:"password_hash"`
 }
